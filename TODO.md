2c6c3a91aa9d21ca140a2672e29f4103e1406882
Mitchell Kember <mk12360@gmail.com>
2017-07-22 19:31:34 -0700

    Port common.lean to Lean 3

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# To do
+
+- Finish porting to Lean 3
+- Replace `suppose` and `take` with `assume` when that change is in stable Lean
+- Replace `let ... := ... in` with `obtain` when it is added back to Lean 3
--- src/common.lean
+++ src/common.lean
@@ -5,24 +5,23 @@ open classical
 
 -- De Morgan's laws
 section de_morgan
-  premises {p q : Prop}
+  variables {p q : Prop}
 
   theorem dm_not_or_not (H : ¬ (p ∧ q)) : ¬ p ∨ ¬ q :=
     by_cases
       (suppose p,
         have ¬ q, from
           suppose q,
-          absurd (and.intro `p` `q`) H,
+          absurd (and.intro ‹p› ‹q›) H,
         show ¬ p ∨ ¬ q, from or.inr this)
       (suppose ¬ p,
         show ¬ p ∨ ¬ q, from or.inl this)
 
   theorem dm_not_and (H : ¬ p ∨ ¬ q) : ¬ (p ∧ q) :=
-    suppose p ∧ q,
+    assume Hpq : p ∧ q,
     show false, from or.elim H
-      (suppose ¬ p, absurd (and.left `p ∧ q`) this)
-      (suppose ¬ q, absurd (and.right `p ∧ q`) this)
-        
+      (suppose ¬ p, absurd Hpq.left this)
+      (suppose ¬ q, absurd Hpq.right this)
 
   theorem dm_not_and_not (H : ¬ (p ∨ q)) : ¬ p ∧ ¬ q :=
     by_cases
@@ -32,33 +31,33 @@ section de_morgan
         have ¬ q, from
           suppose q,
           absurd (or.inr this) H,
-        show ¬ p ∧ ¬ q, from and.intro `¬ p` `¬ q`)
+        show ¬ p ∧ ¬ q, from and.intro ‹¬ p› ‹¬ q›)
 
   theorem dm_not_or (H : ¬ p ∧ ¬ q) : ¬ (p ∨ q) :=
     suppose p ∨ q,
     show false, from or.elim this
-      (suppose p, absurd this (and.left H))
-      (suppose q, absurd this (and.right H))
+      (suppose p, absurd this H.left)
+      (suppose q, absurd this H.right)
 end de_morgan
 
 -- De Morgan's laws (general)
 section de_morgan_general
-  premises {A : Type} {p : A → Prop}
+  variables {A : Type} {p : A → Prop}
 
   theorem dm_exists_not (H : ¬ ∀ a : A, p a) : ∃ a : A, ¬ p a :=
     by_contradiction
-      (assume H1 : ¬ ∃ a : A, ¬ p a,
+      (assume H₁ : ¬ ∃ a : A, ¬ p a,
         have ∀ a : A, p a, from
           take a,
           show p a, from by_contradiction
             (suppose ¬ p a,
-              absurd (exists.intro a this) H1),
+              absurd (exists.intro a this) H₁),
         absurd this H)
 
   theorem dm_not_forall (H : ∃ a : A, ¬ p a) : ¬ ∀ a : A, p a :=
-    obtain (a : A) (H1 : ¬ p a), from H,
+    let ⟨a, H₁⟩ := H in
     suppose ∀ a : A, p a,
-      absurd (this a) H1
+    absurd (this a) H₁
 
   theorem dm_forall_not (H : ¬ ∃ a : A, p a) : ∀ a : A, ¬ p a :=
     take a,
@@ -68,6 +67,6 @@ section de_morgan_general
 
   theorem dm_not_exists (H : ∀ a : A, ¬ p a) : ¬ ∃ a : A, p a :=
     suppose ∃ a : A, p a,
-    obtain (a : A) (H1 : p a), from this,
-    absurd H1 (H a)
+    let ⟨a, H₁⟩ := this in
+    absurd H₁ (H a)
 end de_morgan_general
