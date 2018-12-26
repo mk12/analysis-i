e1266f843a0f0eec2c00db0287a1c3aea51d8b70
Mitchell Kember <mk12360@gmail.com>
2018-12-26 01:20:20 -0500

    Checkpoint

--- coq/Common.v
+++ coq/Common.v
@@ -3,25 +3,17 @@
 (* Formalization of Analysis I: Common theorems                               *)
 (******************************************************************************)
 
+(* Require Export Coq.Logic.Classical. *)
 Require Export Coq.Setoids.Setoid.
 Require Export Coq.Unicode.Utf8.
-(* Require Export Coq.Logic.Classical. *)
 
 (** Restate the current goal *)
 Ltac show G := tryif change G then idtac else fail 0 "Not the current goal".
-
-(** Superscript -1 for the symmetric equality **)
-(* Notation "H ⁻¹" := (eq_sym H) (at level 100, right associativity). *)
 
 (** Modus tollens *)
 Theorem mt {p q : Prop} (Hpq : p → q) (Hnq : ¬ q) : ¬ p.
 Proof.
   intro Hp.
-  absurd q.
-  - exact Hnq.
-  - exact (Hpq Hp).
+  contradiction Hnq.
+  exact (Hpq Hp).
 Qed.
-
-(** Double negation of eq_refl *)
-Theorem nneq {T : Type} {x : T} : ¬ x ≠ x.
-Proof. intro. contradiction. Qed.
